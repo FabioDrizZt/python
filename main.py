5813a6c56e397e2ad706f010389c5218e95da681
bill gates <billgates@microsoft.com>
2024-05-16 17:48:06 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Tipos de datos
+# Strings o cadenas de caracteres
+nombre = 'fabio'
+apellido = "Argañaraz"
+# number o numeros tanto enteros como reales
+enteros = 1123
+reales = 3.1415
+# Booleanos o logicos
+falso = False
+Verdadero = True
+
+nombre = input('ingrese su nombre')
+print('Hola mundo!')
